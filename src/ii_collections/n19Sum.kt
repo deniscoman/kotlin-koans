3cf5ef9429ce0be205f5fcd09297b4267b62288c
Denis Coman <denis.coman13@gmail.com>
2018-07-20 13:11:24 +0300

    Collections Done

--- src/ii_collections/n19Sum.kt
+++ src/ii_collections/n19Sum.kt
@@ -1,5 +1,8 @@
 package ii_collections
 
+import sun.rmi.runtime.Log
+
+
 fun example6() {
     listOf(1, 3).sum() == 4
     listOf("a", "b", "cc").sumBy { it.length } == 4
@@ -8,5 +11,5 @@ fun example6() {
 fun Customer.getTotalOrderPrice(): Double {
     // Return the sum of prices of all products that a customer has ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return this.orders.sumByDouble { it.products.sumByDouble { it.price } }
 }
