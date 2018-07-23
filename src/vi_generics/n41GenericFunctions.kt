e73e43e62f53ba4c05dd319080b9ea9338f41c09
Denis Coman <denis.coman13@gmail.com>
2018-07-23 15:48:32 +0300

    Generics Done

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -2,6 +2,7 @@ package vi_generics
 
 import util.TODO
 import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 fun task41(): Nothing = TODO(
     """
@@ -19,12 +20,17 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C: MutableCollection<T>> Collection<T>.partitionTo(first: C, second: C, condition: (T) -> Boolean): Pair<C, C> {
+    for (element in this) if (condition(element)) first.add(element) else second.add(element)
+    return Pair(first, second)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    //task41()
+    return partitionTo(ArrayList(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+//    task41()
+    return partitionTo(HashSet(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
